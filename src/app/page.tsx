b7815b295b69658b87b3b3368d4b960501e3c08e
nicholas harper <nicholasharper000@gmail.com>
2024-12-16 08:25:16 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,18 @@ type Content = {
 
 async function getData(){
   const { data, error } = await supabase.from("content").select("*").eq("id", 1);
-
-  console.log('Making request to Supabase');
-
-  if (error) {
+  
+  if (error) 
+  {
     throw error;
   }
-
+  
   return data[0];
 }
 
-export default async function Home() {
-
+export default async function Home() 
+{
   const data : Content = await getData();
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
